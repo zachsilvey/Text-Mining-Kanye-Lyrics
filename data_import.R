408503cf08d68432769e379479805b75ab8af3f3
Zach Silvey <zachsilvey@gmail.com>
2017-11-12 21:56:55 -0800

    Added report
    Added a work-in-progress RMarkdown report

--- data_import.R
+++ data_import.R
@@ -1,16 +1,21 @@
 library(readr)
 library(tidyr)
 library(dplyr)
+library(stringr)
 
 # Import lyrics data from CSV
-lyrics <- read_csv("Data/Kanye Lyrics (alpha2).csv", 
+lyrics <- read_csv("Data/Kanye Lyrics (beta1).csv", 
                    col_types = cols(`Run-time` = col_character())) %>%
   # Convert run-time into integer vector representing seconds
   separate("Run-time", c("minutes", "seconds"), sep = ":", convert = TRUE) %>%
-  mutate("run_time" = (minutes * 60) + seconds)
+  mutate("run_time" = (minutes * 60) + seconds) %>%
+  mutate(Lyrics = str_replace_all(Lyrics, "\\[[^]]*]", "")) %>%
+  select(-minutes, -seconds)
 
 # Remove content between [] from lyrics
-clean_lyrics <- gsub("\\[[^]]*]", "", lyrics$Lyrics)
-lyrics$Lyrics <- clean_lyrics
+# Deprecated, using stringr to do this tidy
+#clean_lyrics <- gsub("\\[[^]]*]", "", lyrics$Lyrics)
+#lyrics$Lyrics <- clean_lyrics
 
+#lyrics$Album <- as.factor(lyrics$Album)
 
--- scratch analysis.R
+++ scratch analysis.R
@@ -1,37 +1,53 @@
 library(tidytext)
-library(dplyr)
-library(ggplot2)
+library(tidyverse)
 
+# Tokenize lyrics by word
 tidy_lyrics <- lyrics %>%
   unnest_tokens(word, Lyrics)
 
+# Count occurances of each word token
 tidy_lyrics %>%
   count(word, sort = TRUE)
 
+# Remove stopwords
+# Count occurances of each word token by album
 tidy_lyrics %>%
   anti_join(stop_words) %>%
   count(Album, word, sort = TRUE)
 
+# Plot top 5 words in each album
+# The ordering of bars does not work as intended because some words
+# are shared among albums
 tidy_lyrics %>%
-  filter(Album != "NA") %>%
   anti_join(stop_words) %>%
+  filter(Album != "NA") %>%
   group_by(Album) %>%
-  count(word, sort = TRUE) %>%
-  top_n(5, n) %>%
-  ungroup() %>%
-  mutate(word = reorder(word, n)) %>%
-  ggplot(aes(word, n, fill = Album)) +
+  count(Album, word, sort = TRUE) %>%
+  top_n(5) %>%
+  ggplot(aes(x = reorder(word, n), n, fill = Album)) +
   geom_col(show.legend = FALSE) +
   facet_wrap(~ Album, ncol = 4, scales = "free") +
   coord_flip()
 
+# This works except some stopwords are showing up
+# I believe this is because of an encoding issue
 tidy_lyrics %>%
+  anti_join(stop_words) %>%
   count(Album, word, sort = TRUE) %>%
   bind_tf_idf(word, Album, n) %>%
   arrange(desc(tf_idf)) %>%
   mutate(word = factor(word, levels = rev(unique(word)))) %>%
-  top_n(20) %>%
+  group_by(Album) %>%
+  top_n(5) %>%
   ggplot(aes(word, tf_idf, fill = Album)) +
-  geom_col() +
-  facet_wrap(~ Album, ncol = 2, scales = "free") +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~ Album, ncol = 4, scales = "free") +
   coord_flip()
+
+# Score net sentiment for each album using bag of words
+# and Bing lexicon
+tidy_lyrics %>%
+  inner_join(get_sentiments("bing")) %>%
+  count(Album, sentiment) %>%
+  spread(sentiment, n, fill = 0) %>%
+  mutate(sentiment = positive - negative)
